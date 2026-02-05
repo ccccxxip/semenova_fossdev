655224af846a5f2cd9a61fec4e39276e8258a015
Алёна Семёнова <alenasemenova@MacBook-Pro-Alena.local>
2026-02-05 14:47:09 +0800

    Remade script.py

---
+++ script.py
@@ -0,0 +1,8 @@
+def sum(a, b):
+    return a+b
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Division by zero is forbidden")
+   if isinstance(a, str) or isinstance(b, str):
+       raise ValueError("Could not devide strings")
+    return a/b
