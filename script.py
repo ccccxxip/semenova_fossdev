4a9dd8edc9dfb9009f0ad3beb9595f8a56a078f2
Алёна Семёнова <alenasemenova@MacBook-Pro-Alena.local>
2026-02-10 14:38:31 +0800

    added list diviaion check

--- script.py
+++ script.py
@@ -5,4 +5,6 @@ def divide(a, b):
         raise ValueError("Division by zero is forbidden")
    if isinstance(a, str) or isinstance(b, str):
        raise ValueError("Could not devide strings")
+  if isinstance (a, list) or isinstance(b, list):
+       raise LalueError("Could not divide lists")
     return a/b
