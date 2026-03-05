bda8ee798eecefbee00f2f1bc12f322921184920
ccccxxip <ccccxxip@gmail.com>
2026-03-05 15:20:33 +0800

    fix setup.py, update income module, update tests

--- testing/frameworks/setup.py
+++ testing/frameworks/setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name = "tax",
     version = "0.0.0", 
-    package_dir = {"testing/frameworks/src"},
-    pacjages = find_packages(where="src")
+    package_dir = {"testing/frameworks/src/tax"},
+    packages = find_packages(where="src")
 )
