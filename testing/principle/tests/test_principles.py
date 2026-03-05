644a863afe32f52c6e29eade7182b696d0f0a721
ccccxxip <ccccxxip@gmail.com>
2026-03-05 12:47:14 +0800

    add src and tests

---
+++ testing/principle/tests/test_principles.py
@@ -0,0 +1,14 @@
+import sys 
+sys.path.append("../src")
+
+#TODO make it with 'clear pip insfall -e'
+
+from math_demo import add
+
+def test_addition():
+    assert 2 + 2 == 4
+    print("test ADDIOTION PASSED")
+
+
+if __name__ == "__main__":
+    test_addition()
