f0cc1065cb559349c389edfc4888182ad78df706
ccccxxip <ccccxxip@gmail.com>
2026-03-10 14:35:26 +0800

    Add TDD ndfl module and tests, update unittest tests

---
+++ testing/tdd/tests/test_bank_ndfl.py
@@ -0,0 +1,15 @@
+
+#TODO make test to obey principles
+
+from ndfl import calculate_ndfl
+
+def test_ndfl_tier_1_basic():
+    assert calculate_ndfl(2000000) == 260000
+def test_ndfl_tier_2_basic():
+    assert calculate_ndfl(4000000) == 552000
+def test_ndfl_tier_3_basic():
+    assert calculate_ndfl(10000000) == 1602000
+def test_ndfl_tier_4_basic():
+    assert calculate_ndfl(30000000) == 5402000
+def test_ndfl_tier_5_basic():
+    assert calculate_ndfl(60_000_000) == 11_602_000
