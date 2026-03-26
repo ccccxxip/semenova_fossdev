488d50e173f946c9f831268d0a76141e0107eb23
ccccxxip <ccccxxip@gmail.com>
2026-03-26 10:27:29 +0800

    собираем все библиотеки из файлов проекта

--- make-project/scripts/check_requirements.py
+++ make-project/scripts/check_requirements.py
@@ -2,3 +2,15 @@ from pathlib import Path
 
 SRC_DIR = Path("src")
 REQ_FILE = Path("requirements.txt")
+
+all_imports = set()
+for py_file in SRC_DIR.rglob("*.py"):
+    with open(py_file) as f:
+        for line in f:
+            line = line.strip()
+            if line.startswith("import "):
+                name = line.split()[1].split(".")[0]
+                all_imports.add(name)
+            elif line.startswith("from "):
+                name = line.split()[1].split(".")[0]
+                all_imports.add(name)
