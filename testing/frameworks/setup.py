7247a2157e7c64989f7f578823a7fcd0516c594c
ccccxxip <ccccxxip@gmail.com>
2026-03-30 17:58:12 +0800

    init pypi structure

--- testing/frameworks/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name = "tax",
-    version = "0.0.0", 
-    package_dir = {"testing/frameworks/src/tax"},
-    packages = find_packages(where="src")
-)
---
+++ tests/test_core.py
@@ -0,0 +1,4 @@
+from pypi.core import hello
+
+def test_hello():
+    assert hello() == "Hello from pyflowkit!"
